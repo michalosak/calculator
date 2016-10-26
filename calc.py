3e364b4fd69bc61a173a1a0284cdf291e2145c72
Michał Osak <osakmichal@gmail.com>
2016-10-26 23:46:26 +0200

    liczby całkowite

--- calc.py
+++ calc.py
@@ -7,6 +7,14 @@ def isanumber(a):
         boola= False
         return False
 
+def isinteger(a):
+    boola= True
+    try:
+        boola=int(a)
+        return True
+    except:
+        boola= False
+        return False
 
 def checkoperator(operator):
     if operator=="+":
@@ -22,13 +30,33 @@ def checkoperator(operator):
 
 
 no1=int(0)
-while (isanumber(no1)==True):
 
-    no1 = input("Enter a number (or a letter to exit): ")
+print("============ WELCOME IN SIMPLE CALCULATOR ============")
+
+
+while (isinteger(no1)==True):
+      
+    no1='' 
+    print()
+
+    while True:
     
-    if (isanumber(no1)==True):
+        if (isinteger(no1)==False):
+           no1 = input("Enter an integer number (or a letter to exit): ")
+        if (isanumber(no1)==True and isinteger(no1)==False):
+           no1 = input("Enter an integer number (or a letter to exit): ")
+        if isinteger(no1)==True:
+           break
+        if isanumber(no1)==False:
+           break
+    
+    if (isanumber(no1)==False):
+        print()
+        print("================ HAVE A NICE DAY! BYE! ===============")
+        break
+    
+    if (isinteger(no1)==True):
         
-          
         while True:
             
             operator=input("Enter an operator: ")
@@ -44,35 +72,38 @@ while (isanumber(no1)==True):
 
         while True:
 
-            if (isanumber(no2)==False):
-                no2 = input("Enter a number: ")
+            if (isinteger(no2)==False):
+                no2 = input("Enter an integer number: ")
 
-            if isanumber(no2)==True and operator!="/":
+            if isinteger(no2)==True and operator!="/":
                 break
 
-            if isanumber(no2)==True and operator=="/" and float(no2)==0:
+            if isinteger(no2)==True and operator=="/" and int(no2)==0:
                 print()
                 print ( "You can't divide by zero!!! ")
                 print()
-                no2 = input("Enter a number (different than 0): ")
+                no2 = input("Enter an integer number (different than 0): ")
 
-            if isanumber(no2)==True and operator=="/" and float(no2)!=0:
+            if isinteger(no2)==True and operator=="/" and int(no2)!=0:
                 break
            
+            sume=int()
         if operator=="+":
-            sume=float(no1)+float(no2)
+            sume=int(no1)+int(no2)
             
 
         if operator=="-":
-            sume=float(no1)-float(no2)
+            sume=int(no1)-int(no2)
             
 
         if operator=="*":
-            sume=float(no1)*float(no2)
+            sume=int(no1)*int(no2)
             
-        if operator=="/" and not float(no2)==0:
-            sume=float(no1)/float(no2)
+        if operator=="/" and not int(no2)==0:
+            sume=int(no1)/int(no2)
         
-        print( )
-        print ("Result: "+ str(sume))
-        print()
+        print("-----------------------------------------------------")
+        print ("Result: "+str(no1) + str(operator) + str(no2) +"="+ str(float(sume)))
+        print("-----------------------------------------------------")
+        
+             
